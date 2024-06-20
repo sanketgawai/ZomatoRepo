2f0044175c63cc553ed493fc4c22d586b9a8e4ae
sanket <sanket@sanket>
2024-06-20 14:05:06 +0530

    add prime no class

---
+++ YZomato/src/test/java/TestClasses/CheckPrimeNo.java
@@ -0,0 +1,27 @@
+package TestClasses;
+
+public class CheckPrimeNo {
+
+	public static void main(String[] args) {
+		
+		int no = 241;
+		int temp =0;
+		for(int i=2;i<=no-1;i++)
+		{
+			if(no%i==0)
+			{
+				temp=temp+1;
+			}
+		}
+		if(temp==0)
+		{
+			System.out.println("prime no");
+			
+		}
+		else
+		{
+			System.out.println("not prime no");
+		}
+
+	}
+}
